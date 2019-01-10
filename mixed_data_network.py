e9cbeb101c5b0fdac9afa004b198e3e4f6ab677d
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-10 15:40:35 -0800

    add changes for mixed data k variables

--- mixed_data_network.py
+++ mixed_data_network.py
@@ -48,10 +48,17 @@ num_cuts = 4
 # Make the columns into categorical variables
 
 ## USE THIS FOR THE K-1 DUMMY VARIABLE TEST
+# X_train = pd.DataFrame(DataSampler.sample(n))
+# X_train.iloc[:,cat_columns] = X_train.iloc[:,cat_columns].apply(lambda x: pd.qcut(x, 4, retbins=False,labels=False), axis=0).astype(str)
+# X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns],drop_first=True, prefix= X_train.iloc[:,cat_columns].columns.values.astype(str).tolist())
+# X_train = pd.concat([X_train_dums.reset_index(drop=True),X_train.drop(cat_columns,axis = 1).reset_index(drop=True)], axis=1)
+
+## USE THIS FOR JUST K DUMMY VARIABLES
 X_train = pd.DataFrame(DataSampler.sample(n))
 X_train.iloc[:,cat_columns] = X_train.iloc[:,cat_columns].apply(lambda x: pd.qcut(x, 4, retbins=False,labels=False), axis=0).astype(str)
-X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns],drop_first=True, prefix= X_train.iloc[:,cat_columns].columns.values.astype(str).tolist())
+X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns], prefix= X_train.iloc[:,cat_columns].columns.values.astype(str).tolist())
 X_train = pd.concat([X_train_dums.reset_index(drop=True),X_train.drop(cat_columns,axis = 1).reset_index(drop=True)], axis=1)
+
 
 # Use the observed frequencies of the different categorical values as the probabilities for the observed distribution
 # Generate gumbel-softmax samples using these observed probabilities (do this for each row or for all of the data?)
