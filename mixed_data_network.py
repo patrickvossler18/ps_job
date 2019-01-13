bc7a5be0566bb46de0a1c932b67111b190fac12b
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-12 18:13:43 -0800

    fix mixed data network not turning the categorical columns to strings

--- mixed_data_network.py
+++ mixed_data_network.py
@@ -55,9 +55,8 @@ num_cuts = 4
 
 ## USE THIS FOR JUST K DUMMY VARIABLES
 X_train = pd.DataFrame(DataSampler.sample(n))
-X_train.iloc[:,cat_columns] = X_train.iloc[:,cat_columns].apply(lambda x: pd.qcut(x, 4, retbins=False,labels=False), axis=0)
-# X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns], prefix= X_train.iloc[:,cat_columns].columns.values.astype(str).tolist())
-X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns])
+X_train.iloc[:,cat_columns] = X_train.iloc[:,cat_columns].apply(lambda x: pd.qcut(x, 4, retbins=False,labels=False), axis=0).astype(str)
+X_train_dums = pd.get_dummies(X_train.iloc[:,cat_columns], prefix= X_train.iloc[:,cat_columns].columns.values.astype(str).tolist())
 X_train = pd.concat([X_train_dums.reset_index(drop=True),X_train.drop(cat_columns,axis = 1).reset_index(drop=True)], axis=1)
 
 
