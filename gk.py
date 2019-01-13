a72fe1833d62ffda74a2883bec00f5be42a737b7
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-12 23:52:28 -0800

    increase the amount we add to Sigma

--- gk.py
+++ gk.py
@@ -23,7 +23,7 @@ def solve_sdp(Sigma, tol=1e-3):
     # Convert the covariance matrix to a correlation matrix
     # Check whether Sigma is positive definite
     if(np.min(np.linalg.eigvals(Sigma))<0):
-        corrMatrix = cov2cor(Sigma + (1e-8)*np.eye(Sigma.shape[0]))
+        corrMatrix = cov2cor(Sigma + (1e-4)*np.eye(Sigma.shape[0]))
     else:
         corrMatrix = cov2cor(Sigma) 
     p,_ = corrMatrix.shape
