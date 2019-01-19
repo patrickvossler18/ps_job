f59ac1786a51340404a6789d0257fc67df8ff0b9
patrickvossler18 <patrick.vossler18@gmail.com>
2019-01-19 15:59:31 -0800

    debug weird error in datasampler

--- gk.py
+++ gk.py
@@ -15,7 +15,7 @@ def cov2cor(Sigma):
     return np.divide(Sigma, scalingFactors)
 
 
-def solve_sdp(Sigma, tol=1e-3):
+def solve_sdp(Sigma, tol=1e-3, regularizer=1e-4):
     """
     Computes s for sdp-correlated Gaussian knockoffs
     :param Sigma : A covariance matrix (p x p)
@@ -25,7 +25,7 @@ def solve_sdp(Sigma, tol=1e-3):
     # Convert the covariance matrix to a correlation matrix
     # Check whether Sigma is positive definite
     if(np.min(np.linalg.eigvals(Sigma)) < 0):
-        corrMatrix = cov2cor(Sigma + (1e-4)*np.eye(Sigma.shape[0]))
+        corrMatrix = cov2cor(Sigma + (regularizer)*np.eye(Sigma.shape[0]))
     else:
         corrMatrix = cov2cor(Sigma)
     p, _ = corrMatrix.shape
@@ -47,7 +47,7 @@ class GaussianKnockoffs:
     Knockoffs for a multivariate Gaussian model
     """
 
-    def __init__(self, Sigma, method="equi", mu=[], tol=1e-3):
+    def __init__(self, Sigma, method="equi", mu=[], tol=1e-3, regularizer=1e-4):
         """
         Constructor
         :param model  : A multivariate Gaussian model object containing the covariance matrix
@@ -70,7 +70,7 @@ class GaussianKnockoffs:
             s = min(1, 2*(lambda_min-tol))
             self.Ds = np.diag([s]*self.Sigma.shape[0])
         elif self.method == "sdp":
-            self.Ds = np.diag(solve_sdp(self.Sigma, tol=tol))
+            self.Ds = np.diag(solve_sdp(self.Sigma, tol=tol, regularizer=regularizer))
         else:
             raise ValueError('Invalid Gaussian knockoff type: '+self.method)
         self.SigmaInvDs = linalg.lstsq(self.Sigma, self.Ds)[0]
