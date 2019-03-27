c0faacd7c2a65771e2370832a744cff8024a7b53
patrickvossler18 <patrick.vossler18@gmail.com>
2019-03-27 12:24:06 -0700

    Add cpp_data_network

---
+++ cpp_data_network.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+from DeepKnockoffs import KnockoffMachine
+# from DeepKnockoffs import GaussianKnockoffs
+import gk
+import data
+import parameters
+from sklearn.covariance import MinCovDet, LedoitWolf
+
+
+
+# Load the built-in multivariate Student's-t model and its default parameters
+# The currently available built-in models are:
+# - gaussian : Multivariate Gaussian distribution
+# - gmm      : Gaussian mixture model
+# - mstudent : Multivariate Student's-t distribution
+# - sparse   : Multivariate sparse Gaussian distribution
+# model = "mixed_student"
+model = "gaussian"
+
+# Load data
+X = pd.read_csv("cpp_final.csv")
+
+
+# Split train test 80-20 and save index of train data for later
+X_train = X.sample(frac=0.8,random_state=200)
+np.savetxt("/artifacts/train_msk.csv", X_train.index, delimiter=",")
+
+# Regularize the covariance and generate second order knockoffs
+mcd = MinCovDet().fit(X_train)
+SigmaHat_mcd = mcd.covariance_ 
+second_order = gk.GaussianKnockoffs(SigmaHat_mcd, mu=np.mean(X_train, 0), method="sdp", regularizer=0.09)
+corr_g = (np.diag(SigmaHat_mcd) - np.diag(second_order.Ds)) / np.diag(SigmaHat_mcd)
+
+print(np.average(corr_g))
+
+training_params = parameters.GetTrainingHyperParams(model)
+p = X_train.shape[1]
+
+# Set the parameters for training deep knockoffs
+pars = dict()
+# Number of epochs
+pars['epochs'] = 100
+# Number of iterations over the full data per epoch
+pars['epoch_length'] = 100
+# Data type, either "continuous" or "binary"
+pars['family'] = "continuous"
+# Dimensions of the data
+pars['p'] = p
+pars['ncat'] = ncat
+# List of categorical variables
+pars['cat_var_idx'] = np.arange(0, (ncat * (num_cuts)))
+# Number of discrete variables
+pars['ncat'] = ncat
+# Number of categories
+pars['num_cuts'] = num_cuts
+# Size of regularizer
+# pars['regularizer'] = grid_results[0]
+# Boolean for using different weighting structure for decorr
+pars['use_weighting'] = False
+# Multiplier for weighting discrete variables
+pars['kappa'] = 1
+# Boolean for using the different decorr loss function from the paper
+pars['diff_decorr'] = False
+# Boolean for using mixed data in forward function
+pars['mixed_data'] = True
+# Size of the test set
+pars['test_size'] = 0
+# Batch size
+pars['batch_size'] = int(0.5*n)
+# Learning rate
+pars['lr'] = 0.01
+# When to decrease learning rate (unused when equal to number of epochs)
+pars['lr_milestones'] = [pars['epochs']]
+# Width of the network (number of layers is fixed to 6)
+pars['dim_h'] = int(10*p)
+# Penalty for the MMD distance
+pars['GAMMA'] = training_params['GAMMA']
+# Penalty encouraging second-order knockoffs
+pars['LAMBDA'] = training_params['LAMBDA']
+# Decorrelation penalty hyperparameter
+pars['DELTA'] = training_params['DELTA']
+# Target pairwise correlations between variables and knockoffs
+pars['target_corr'] = corr_g
+# Kernel widths for the MMD measure (uniform weights)
+pars['alphas'] = [1., 2., 4., 8., 16., 32., 64., 128.]
+
+# machine = KnockoffMachine(pars)
+# machine.train(X_train.values)
+
+# Save parameters
+np.save('/artifacts/pars.npy', pars)
+
+
+# Where to store the machine
+checkpoint_name = "/artifacts/" + model
+
+# Where to print progress information
+logs_name = "/artifacts/" + model + "_progress.txt"
+
+# Initialize the machine
+machine = KnockoffMachine(pars, checkpoint_name=checkpoint_name, logs_name=logs_name)
+
+# Train the machine
+machine.train(X_train.values)
