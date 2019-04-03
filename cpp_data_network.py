eef45ccad9ff409da51924d66b0342b5c65c0ac5
patrickvossler18 <patrick.vossler18@gmail.com>
2019-04-03 12:13:03 -0700

    try smaller regularizer

--- cpp_data_network.py
+++ cpp_data_network.py
@@ -51,7 +51,7 @@ np.savetxt("/artifacts/train_msk.csv", X_train.index, delimiter=",")
 mcd = LedoitWolf().fit(X_train)
 SigmaHat_mcd = mcd.covariance_ 
 # SigmaHat_mcd = np.cov(X_train, rowvar=False)
-second_order = gk.GaussianKnockoffs(SigmaHat_mcd, mu=np.mean(X_train, 0), method="sdp", regularizer=1e-2)
+second_order = gk.GaussianKnockoffs(SigmaHat_mcd, mu=np.mean(X_train, 0), method="sdp", regularizer=1e-3)
 corr_g = (np.diag(SigmaHat_mcd) - np.diag(second_order.Ds)) / np.diag(SigmaHat_mcd)
 
 print(np.average(corr_g))
