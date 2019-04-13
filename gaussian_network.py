679ef470ceb0a06d19a62c7f731cd46dc3c8089f
patrickvossler18 <patrick.vossler18@gmail.com>
2019-04-12 22:06:28 -0700

    try with just regularization

--- gaussian_network.py
+++ gaussian_network.py
@@ -32,9 +32,9 @@ num_cuts = 4
 X_train = DataSampler.sample(n)
 
 # SigmaHat = np.cov(X_train, rowvar=False)
-mcd = MinCovDet().fit(X_train)
-SigmaHat = mcd.covariance_ 
-SigmaHat= SigmaHat + (1e-1)*np.eye(SigmaHat.shape[0])
+# mcd = MinCovDet().fit(X_train)
+# SigmaHat = mcd.covariance_ 
+SigmaHat= SigmaHat + (2e-1)*np.eye(SigmaHat.shape[0])
 
 # TO USE LATER
 # regularizer = np.array([1e-1]*(num_cuts*ncat)+[0]*(SigmaHat.shape[1]-(num_cuts*ncat)))
