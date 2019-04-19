5809fd616caeca7315194ffcba54f305595cc864
patrickvossler18 <patrick.vossler18@gmail.com>
2019-04-18 18:19:08 -0700

    Try using MCD and do nan_to_num for 0 correlations

--- cpp_data_network.py
+++ cpp_data_network.py
@@ -39,14 +39,13 @@ X_train = X.sample(frac=0.8,random_state=200)
 np.savetxt("/artifacts/train_msk.csv", X_train.index, delimiter=",")
 
 # Regularize the covariance and generate second order knockoffs
-# mcd = MinCovDet().fit(X_train)
-SigmaHat_mcd = ledoit_wolf(X_train)[0]
-# SigmaHat_mcd = mcd.covariance_ 
+mcd = MinCovDet().fit(X_train)
+# SigmaHat_mcd = ledoit_wolf(X_train)[0]
+SigmaHat_mcd = mcd.covariance_ 
 # SigmaHat_mcd[SigmaHat_mcd ==0] = 1e-13
 # SigmaHat_mcd = np.cov(X_train, rowvar=False)
-second_order = gk.GaussianKnockoffs(SigmaHat_mcd, mu=np.mean(X_train, 0), method="sdp", regularizer=0.02)
-# corr_g = (np.diag(SigmaHat_mcd) - np.diag(second_order.Ds)) / np.diag(SigmaHat_mcd)
-corr_g = np.diag(second_order.Ds)
+second_order = gk.GaussianKnockoffs(SigmaHat_mcd, mu=np.mean(X_train, 0), method="sdp", regularizer=0.01)
+corr_g = np.nan_to_num((np.diag(SigmaHat_mcd) - np.diag(second_order.Ds)) / np.diag(SigmaHat_mcd))
 
 print(np.average(corr_g))
 
