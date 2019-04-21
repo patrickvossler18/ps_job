33f567932fe7793bea6e3fc87a0a3d6b01aa5acc
patrickvossler18 <patrick.vossler18@gmail.com>
2019-04-21 16:39:34 -0700

    With the additional noise variables we get a low average correlation

--- cpp_data_network.py
+++ cpp_data_network.py
@@ -46,7 +46,7 @@ mcd = MinCovDet().fit(X_train)
 # SigmaHat_mcd = ledoit_wolf(X_train)[0]
 SigmaHat_mcd = mcd.covariance_ 
 if(np.min(np.linalg.eigvals(SigmaHat_mcd)) < 0):
-    SigmaHat_mcd = SigmaHat_mcd + (5e-4)*np.eye(SigmaHat_mcd.shape[0])
+    SigmaHat_mcd = SigmaHat_mcd + (3e-4)*np.eye(SigmaHat_mcd.shape[0])
 
 X_train = X_train.values
 # myScaler = preprocessing.StandardScaler()
@@ -62,7 +62,7 @@ X_train = X_train.values
 
 SigmaHat = SigmaHat_mcd
 # second_order = gk.GaussianKnockoffs(SigmaHat, mu=np.mean(X_train, 0), method="sdp", regularizer=0.001)
-second_order = gk.GaussianKnockoffs(SigmaHat, mu=np.mean(X_train, 0), method="sdp", regularizer=0.02)
+second_order = gk.GaussianKnockoffs(SigmaHat, mu=np.mean(X_train, 0), method="sdp", regularizer=0.001)
 corr_g = np.nan_to_num((np.diag(SigmaHat) - np.diag(second_order.Ds)) / np.diag(SigmaHat))
 
 print(np.average(corr_g))
