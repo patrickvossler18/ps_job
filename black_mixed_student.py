694e7dda328793a169419dff4921eacbbb1a65b7
patrickvossler18 <patrick.vossler18@gmail.com>
2019-05-01 11:11:26 -0700

    fix black data

--- black_mixed_student.py
+++ black_mixed_student.py
@@ -106,9 +106,10 @@ pars['mixed_data'] = True
 # Size of the test set
 pars['test_size'] = 0
 # Batch size
-pars['batch_size'] = int(0.3*n)
+# pars['batch_size'] = int(0.2*n)
+pars['batch_size'] = 100
 # Learning rate
-pars['lr'] = 0.01
+pars['lr'] = 0.001
 # When to decrease learning rate (unused when equal to number of epochs)
 pars['lr_milestones'] = [pars['epochs']]
 # Width of the network (number of layers is fixed to 6)
