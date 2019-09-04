c56499f862bef44b9f354b3ad569485bfc578a95
patrickvossler18 <patrick.vossler18@gmail.com>
2019-09-04 09:49:00 -0700

    Use 8e-3 for p=300,500 student's t

--- student_network_asdp.py
+++ student_network_asdp.py
@@ -44,7 +44,7 @@ for p_size in [300,500]:
     SigmaHat = np.cov(X_train, rowvar=False)
     # mcd = MinCovDet().fit(X_train)
     # SigmaHat = mcd.covariance_ 
-    SigmaHat= SigmaHat + (2e-2)*np.eye(SigmaHat.shape[0])
+    SigmaHat= SigmaHat + (8e-3)*np.eye(SigmaHat.shape[0])
 
     # TO USE LATER
     # regularizer = np.array([1e-1]*(num_cuts*ncat)+[0]*(SigmaHat.shape[1]-(num_cuts*ncat)))
