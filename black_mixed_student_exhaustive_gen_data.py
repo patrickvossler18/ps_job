6d73fcded749916fd5670aba94e1f117fef72720
patrickvossler18 <patrick.vossler18@gmail.com>
2019-09-20 15:07:38 -0700

    add the birth weight training files for the joint method

---
+++ black_mixed_student_exhaustive_gen_data.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from DeepKnockoffs import KnockoffMachine
+from DeepKnockoffs import GaussianKnockoffs
+import sys
+sys.path.insert(1, "/home/pvossler/ps_job")
+import data
+import parameters
+from sklearn.covariance import MinCovDet, LedoitWolf
+from sklearn import preprocessing, covariance
+from itertools import chain
+import utils
+import datetime 
+
+now = datetime.datetime.now()
+timestamp = now.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (now.microsecond / 10000))
+
+MODEL_DIRECTORY = "/home/pvossler/cm_idea/black_test/"
+black_train_msk_path = MODEL_DIRECTORY+"black_train_msk.csv"
+# The currently available built-in models are:
+# - gaussian : Multivariate Gaussian distribution
+# - gmm      : Gaussian mixture model
+# - mstudent : Multivariate Student's-t distribution
+# - sparse   : Multivariate sparse Gaussian distribution
+# model = "mixed_student"
+model = "mixed_student"
+
+# Load data
+black_data = pd.read_csv("black_clean.csv")
+
+factor_list = pd.read_csv("black_factor_list.csv").values.tolist()
+chunk_list = pd.read_csv("black_chunk_list.csv").values.tolist()
+cat_var_idx = pd.read_csv("black_cat_var_idx.csv").values.tolist()
+factor_list = list(chain(*factor_list))
+chunk_list = list(chain(*chunk_list))
+cat_var_idx = list(chain(*cat_var_idx))
+cat_var_idx = np.array(cat_var_idx) - 1
+
+
+
+# Drop Y and W
+X =  black_data.drop(columns=["Y","W"])
+
+
+# Split train test 80-20 and save index of train data for later
+X_train = X.sample(frac=0.8,random_state=200)
+np.savetxt(black_train_msk_path, X_train.index, delimiter=",")
+
+# Regularize the covariance and generate second order knockoffs
+if robust_cov:
+    mcd = MinCovDet().fit(X_train)
+    SigmaHat = mcd.covariance_ 
+else:
+    SigmaHat = np.cov(X_train, rowvar=False)
+
+# reg_val = 5e-3
+SigmaHat= SigmaHat + (reg_val)*np.eye(SigmaHat.shape[0])
+
+if p > 100:
+    identity_p = np.identity(SigmaHat.shape[1])
+    Sigma_inv = np.linalg.solve(SigmaHat, identity_p)
+
+    def ASDPoptim(Sigma, Sigma_inv, block_size, approx_method):
+            sol = utils.asdp(Sigma, block_size, approx_method)
+            s = np.diag(np.array(sol).flatten())
+            C2 = 2. * s - np.dot(s, np.dot(Sigma_inv, s))        
+            return(C2, s)
+
+    C2, s = ASDPoptim(SigmaHat, Sigma_inv,50, approx_method="selfblocks")
+
+
+    Ds = np.multiply(s,np.diag(SigmaHat))
+
+    corr_g = (np.diag(SigmaHat) - np.diag(Ds)) / np.diag(SigmaHat)
+
+else:
+    second_order = GaussianKnockoffs(SigmaHat, mu=np.mean(X_train, 0), method="sdp")
+
+    # Measure pairwise second-order knockoff correlations
+    corr_g = (np.diag(SigmaHat) - np.diag(second_order.Ds)) / np.diag(SigmaHat)
+
+print(np.average(corr_g))
+
+print(np.average(corr_g[1:(np.sum(num_cuts)-1)]))
+print(np.average(corr_g[((np.sum(num_cuts)-1)+1):]))
+
+avg_corr = np.average(corr_g)
+
+avg_corr_cat = np.average(corr_g[1:(np.sum(num_cuts)-1)])
+avg_corr_cont = np.average(corr_g[((np.sum(num_cuts)-1)+1):])
+
---
+++ black_mixed_student_joint_method.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from DeepKnockoffs import KnockoffMachine
+from DeepKnockoffs import GaussianKnockoffs
+import sys
+sys.path.insert(1, "/home/pvossler/ps_job")
+# sys.path.append("/home/pvossler/ps_job")
+import data
+import parameters
+from sklearn.covariance import MinCovDet, LedoitWolf
+import utils
+import datetime 
+
+
+
+model="mstudent"
+training_params = parameters.GetTrainingHyperParams(model)
+training_params['LAMBDA'] = lambda_val
+training_params['DELTA'] = delta_val
+
+
+p = X_train.shape[1]
+
+# Set the parameters for training deep knockoffs
+pars = dict()
+
+pars['avg_corr'] = avg_corr
+pars['avg_corr_cat'] = avg_corr_cat
+pars['avg_corr_cont'] = avg_corr_cont
+# Number of epochs
+pars['epochs'] = 50
+# Number of iterations over the full data per epoch
+pars['epoch_length'] = 50
+# Data type, either "continuous" or "binary"
+pars['family'] = "continuous"
+# Dimensions of the data
+pars['p'] = p
+# List of categorical variables
+pars['cat_var_idx'] = cat_var_idx
+# Number of categories for each categorical variable
+pars['chunk_list'] = chunk_list
+# # Number of categories
+# pars['num_cuts'] = num_cuts
+# Size of regularizer
+# pars['regularizer'] = grid_results[0]
+# Boolean for using different weighting structure for decorr
+pars['use_weighting'] = False
+# Multiplier for weighting discrete variables
+pars['kappa'] = 1
+# Boolean for using the different decorr loss function from the paper
+pars['diff_decorr'] = False
+# Boolean for using mixed data in forward function
+pars['mixed_data'] = True
+# Size of the test set
+pars['test_size'] = 0
+# Batch size
+pars['batch_size'] = int(0.2*n)
+# Learning rate
+pars['lr'] = 0.01
+# When to decrease learning rate (unused when equal to number of epochs)
+pars['lr_milestones'] = [pars['epochs']]
+# Width of the network (number of layers is fixed to 6)
+pars['dim_h'] = int(10*p)
+# Penalty for the MMD distance
+pars['GAMMA'] = training_params['GAMMA']
+# Penalty encouraging second-order knockoffs
+pars['LAMBDA'] = training_params['LAMBDA']
+# Decorrelation penalty hyperparameter
+pars['DELTA'] = training_params['DELTA']
+# Target pairwise correlations between variables and knockoffs
+pars['target_corr'] = corr_g
+# Kernel widths for the MMD measure (uniform weights)
+pars['alphas'] = [1., 2., 4., 8., 16., 32., 64., 128.]
+
+pars_name = MODEL_DIRECTORY + 'pars' + timestamp + '.npy'
+# Save parameters
+np.save(pars_name, pars)
+
+# Where to store the machine
+checkpoint_name = MODEL_DIRECTORY + model + timestamp
+
+# Where to print progress information
+logs_name = MODEL_DIRECTORY + model + timestamp + "_progress.txt"
+
+# Initialize the machine
+machine = KnockoffMachine(pars, checkpoint_name=checkpoint_name, logs_name=logs_name)
+
+# Train the machine
+machine.train(X_train.values)
+
+print(timestamp)
