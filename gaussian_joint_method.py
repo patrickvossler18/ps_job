c4532411f9012ce4bb14ee8b7ffb6bce4b879bf9
patrickvossler18 <patrick.vossler18@gmail.com>
2019-09-23 16:55:56 -0700

    Add updated joint gaussian method that allows for robust covariance

---
+++ gaussian_joint_method.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+from DeepKnockoffs import KnockoffMachine
+from DeepKnockoffs import GaussianKnockoffs
+import sys
+sys.path.insert(1, "/home/pvossler/ps_job")
+# sys.path.append("/home/pvossler/ps_job")
+import data
+import parameters
+from sklearn.covariance import MinCovDet, LedoitWolf
+import utils
+import datetime 
+
+
+
+p = int(p_arg)
+now = datetime.datetime.now()
+timestamp = now.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (now.microsecond / 10000))
+
+MODEL_DIRECTORY = "/home/pvossler/cm_idea/"
+print(p)
+p_size = p
+# Load the built-in multivariate Student's-t model and its default parameters
+# The currently available built-in models are:
+# - gaussian : Multivariate Gaussian distribution
+# - gmm      : Gaussian mixture model
+# - mstudent : Multivariate Student's-t distribution
+# - sparse   : Multivariate sparse Gaussian distribution
+# model = "mixed_student"
+model = "gaussian"
+distribution_params = parameters.GetDistributionParams(model, p)
+
+# Initialize the data generator
+DataSampler = data.DataSampler(distribution_params)
+
+# Number of training examples
+n = 1000
+
+
+# USE THIS FOR JUST K DUMMY VARIABLES
+X_train = pd.DataFrame(DataSampler.sample(n))
+
+if robust_cov:
+    mcd = MinCovDet().fit(X_train)
+    SigmaHat = mcd.covariance_ 
+else:
+    SigmaHat = np.cov(X_train, rowvar=False)
+
+# reg_val = 5e-3
+SigmaHat= SigmaHat + (reg_val)*np.eye(SigmaHat.shape[0])
+
+
+if p > 100:
+    identity_p = np.identity(SigmaHat.shape[1])
+    Sigma_inv = np.linalg.solve(SigmaHat, identity_p)
+
+    def ASDPoptim(Sigma, Sigma_inv, block_size, approx_method):
+            sol = utils.asdp(Sigma, block_size, approx_method)
+            s = np.diag(np.array(sol).flatten())
+            C2 = 2. * s - np.dot(s, np.dot(Sigma_inv, s))        
+            return(C2, s)
+
+    C2, s = ASDPoptim(SigmaHat, Sigma_inv,50, approx_method="selfblocks")
+
+
+    Ds = np.multiply(s,np.diag(SigmaHat))
+
+    corr_g = (np.diag(SigmaHat) - np.diag(Ds)) / np.diag(SigmaHat)
+
+else:
+    second_order = GaussianKnockoffs(SigmaHat, mu=np.mean(X_train, 0), method="sdp")
+
+    # Measure pairwise second-order knockoff correlations
+    corr_g = (np.diag(SigmaHat) - np.diag(second_order.Ds)) / np.diag(SigmaHat)
+
+
+print(np.average(corr_g))
+
+# Set the parameters for training deep knockoffs
+pars = dict()
+# Number of epochs
+pars['epochs'] = 25
+# Number of iterations over the full data per epoch
+pars['epoch_length'] = 100
+# Data type, either "continuous" or "binary"
+pars['family'] = "continuous"
+# Dimensions of the data
+pars['p'] = p
+# pars['ncat'] = ncat
+# # List of categorical variables
+# pars['cat_var_idx'] = np.arange(0, (ncat * (num_cuts)))
+# # Number of discrete variables
+# pars['ncat'] = ncat
+# # Number of categories
+# pars['num_cuts'] = num_cuts
+# # Size of regularizer
+# # pars['regularizer'] = grid_results[0]
+# # Boolean for using different weighting structure for decorr
+# pars['use_weighting'] = False
+# # Multiplier for weighting discrete variables
+# pars['kappa'] = 1
+# # Boolean for using the different decorr loss function from the paper
+# pars['diff_decorr'] = False
+# # Boolean for using mixed data in forward function
+# pars['mixed_data'] = False
+# Size of the test set
+pars['test_size'] = 0
+# Batch size
+pars['batch_size'] = int(0.5*n)
+# Learning rate
+pars['lr'] = 0.001
+# When to decrease learning rate (unused when equal to number of epochs)
+pars['lr_milestones'] = [pars['epochs']]
+# Width of the network (number of layers is fixed to 6)
+pars['dim_h'] = int(10*p)
+# Penalty for the MMD distance
+pars['GAMMA'] = training_params['GAMMA']
+# Penalty encouraging second-order knockoffs
+pars['LAMBDA'] = training_params['LAMBDA']
+# Decorrelation penalty hyperparameter
+pars['DELTA'] = training_params['DELTA']
+# Target pairwise correlations between variables and knockoffs
+pars['target_corr'] = corr_g
+# Kernel widths for the MMD measure (uniform weights)
+pars['alphas'] = [1., 2., 4., 8., 16., 32., 64., 128.]
+
+
+
+pars_name = MODEL_DIRECTORY + 'pars' + timestamp + '.npy'
+# Save parameters
+np.save(pars_name, pars)
+
+# Where to store the machine
+checkpoint_name = MODEL_DIRECTORY + model + timestamp
+
+# Where to print progress information
+logs_name = MODEL_DIRECTORY + model + timestamp + "_progress.txt"
+
+# Initialize the machine
+machine = KnockoffMachine(pars, checkpoint_name=checkpoint_name, logs_name=logs_name)
+
+# Train the machine
+machine.train(X_train.values)
+
+print(timestamp)
