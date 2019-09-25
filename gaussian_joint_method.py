34311ee027cb481017cc81e1839304609878935a
patrickvossler18 <patrick.vossler18@gmail.com>
2019-09-25 16:51:31 -0700

    fix the learning rate

--- gaussian_joint_method.py
+++ gaussian_joint_method.py
@@ -113,7 +113,7 @@ pars['test_size'] = 0
 # Batch size
 pars['batch_size'] = int(0.5*n)
 # Learning rate
-pars['lr'] = 0.001
+pars['lr'] = 0.01
 # When to decrease learning rate (unused when equal to number of epochs)
 pars['lr_milestones'] = [pars['epochs']]
 # Width of the network (number of layers is fixed to 6)
