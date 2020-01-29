ba170d1d8883e4abfea7109e20d978f19269cf23
Patrick Vossler <patrick.vossler18@gmail.com>
2020-01-28 17:34:48 -0800

    back to normal learning rate

--- test_rfvarsel_joint_method.py
+++ test_rfvarsel_joint_method.py
@@ -54,7 +54,7 @@ pars['test_size'] = 0
 # Batch size
 pars['batch_size'] = int(0.2*n)
 # Learning rate
-pars['lr'] = 0.05
+pars['lr'] = 0.01
 # When to decrease learning rate (unused when equal to number of epochs)
 pars['lr_milestones'] = [pars['epochs']]
 # Width of the network (number of layers is fixed to 6)
